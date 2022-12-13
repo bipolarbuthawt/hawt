e93363166223e5035b45e863373e50c5afc8e1c0
bipolarbuthawt <120359716+bipolarbuthawt@users.noreply.github.com>
2022-12-13 03:13:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// GOOGLE ANALYTICS
-/* dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=G-1GYXPR2X6G")
+<!-- Google Analytics -->
+dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=G-1GYXPR2X6G")
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
@@ -8,15 +8,8 @@ gtag('js', new Date());
 gtag('config', 'G-1PBQ77YD00', {
     page_title: 'Main',
     page_location: 'https://bipolarbuthawt.tk'
-}); */
-
-<!-- Google Tag Manager -->
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T9ZDMQL');
-<!-- End Google Tag Manager -->
+});
+<!-- End Google Analytics -->
 
 // REDIRECT
 // window.location.replace("https://onlyfans.com/bipolarbuthawt");
