cfeafbeccd2ec5a2d50edf5f1b0d359cc8f03acd
bipolarbuthawt <120359716+bipolarbuthawt@users.noreply.github.com>
2022-12-13 00:58:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-1PBQ77YD00', page_title: 'Main');
+gtag('config', 'G-1PBQ77YD00', page_title: 'Main', page_location: 'https://bipolarbuthawt.tk');
 
 // REDIRECT
 window.location.replace("https://onlyfans.com/bipolarbuthawt");
