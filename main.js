98f4a67900f6052fb7863879ee8786e2d5869972
bipolarbuthawt <120359716+bipolarbuthawt@users.noreply.github.com>
2022-12-13 02:58:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-function dynamicallyLoadScript(url) {
-    var script = document.createElement("script");
-    script.async = true;
-    script.src = url;
-
-    document.head.appendChild(script);
-}
-
 // GOOGLE ANALYTICS
-dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=G-1GYXPR2X6G")
+/* dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=G-1GYXPR2X6G")
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
@@ -16,7 +8,15 @@ gtag('js', new Date());
 gtag('config', 'G-1PBQ77YD00', {
     page_title: 'Main',
     page_location: 'https://bipolarbuthawt.tk'
-});
+}); */
+
+<!-- Google Tag Manager -->
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T9ZDMQL');
+<!-- End Google Tag Manager -->
 
 // REDIRECT
 // window.location.replace("https://onlyfans.com/bipolarbuthawt");
