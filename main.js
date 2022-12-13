6650083513c08b32e6677f4114769e66ef424fe9
bipolarbuthawt <120359716+bipolarbuthawt@users.noreply.github.com>
2022-12-12 17:31:13 -0700

    ss

---
+++ main.js
@@ -0,0 +1,19 @@
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = url;
+
+    document.head.appendChild(script);
+}
+
+// GOOGLE ANALYTICS
+dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=G-1GYXPR2X6G")
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-1PBQ77YD00');
+
+// REDIRECT
+window.location.replace("https://onlyfans.com/bipolarbuthawt");
