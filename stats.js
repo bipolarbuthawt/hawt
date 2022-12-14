06e6e5242195dce821fadc15ec907c652428e8de
bipolarbuthawt <120359716+bipolarbuthawt@users.noreply.github.com>
2022-12-13 21:33:52 -0700

    Fix for liveViews function error

--- stats.js
+++ stats.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.countapi.xyz/hit/bipolarbuthawt.tk/Main");
+xhr.open("GET", "https://api.countapi.xyz/hit/bipolarbuthawt.tk/Main/");
 xhr.responseType = "json";
 xhr.onload = function() {
     alert(`Main has ${this.response.value} views!`);
