74a16c84b68313b318861105549da4d5365ed220
bipolarbuthawt <120359716+bipolarbuthawt@users.noreply.github.com>
2022-12-13 19:52:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,13 +12,11 @@ function updateViews(response) {
     document.body.appendChild(p);
 }
 
-// Google Analytics
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-1PBQ77YD00');
-// End Google Analytics
 
 // Counter API
 dynamicallyLoadScript("https://api.countapi.xyz/hit/bipolarbuthawt.tk/?callback=updateViews");
