e2158da797e9c8d5af380c904599bb8cc32e6910
bipolarbuthawt <120359716+bipolarbuthawt@users.noreply.github.com>
2022-12-13 19:29:07 -0700

    Added counter api and test element to view it

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-<!-- Google Analytics -->
-dynamicallyLoadScript("https://www.googletagmanager.com/gtag/js?id=G-1GYXPR2X6G")
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = url;
 
+    document.head.appendChild(script);
+}
+
+function updateViews(response) {
+    var p = document.createElement("p");
+    p.innerText = response.value;
+    document.body.appendChild(p);
+}
+
+<!-- Google Analytics -->
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
@@ -8,5 +20,8 @@ gtag('js', new Date());
 gtag('config', 'G-1PBQ77YD00');
 <!-- End Google Analytics -->
 
-// REDIRECT
+// Counter API
+dynamicallyLoadScript("https://api.countapi.xyz/hit/bipolarbuthawt.tk/?callback=updateViews");
+
+// Redirect
 // window.location.replace("https://onlyfans.com/bipolarbuthawt");
